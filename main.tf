db60cff7bd8eb888ab092bfa93be33ff837ceb59
Dakota Gallimore <gallimoredakota@gmail.com>
2021-03-08 00:45:14 -0500

    Add AWS S3 read IAM policy

--- main.tf
+++ main.tf
@@ -12,3 +12,33 @@ versioning {
     ManagedBy      = "Terraform"
   }
 }
+
+resource "aws_iam_policy" "s3_read_policy" {
+  name        = "s3_read_policy"
+  path        = "/"
+  description = "Policy allowing AWS resources to read S3 bucket contents for ${aws_s3_bucket.portfolio_data_files_bucket.bucket} bucket"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+            "logs:PutLogEvents",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action = [
+            "s3:GetObject",
+        ]
+        Effect = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.portfolio_data_files_bucket.bucket}/*"
+      }
+    ]
+  })
+}
